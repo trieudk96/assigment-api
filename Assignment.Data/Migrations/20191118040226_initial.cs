ab3b0271b86f478fde9c025821ce887f3247868a
dktrieu <dktrieu@cmc.com.vn>
2019-11-18 11:22:13 +0700

    add migration

---
+++ Assignment.Data/Migrations/20191118040226_initial.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Assignment.Data.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "dbo");
+
+            migrationBuilder.CreateTable(
+                name: "USER_ASSIGNMENT",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(maxLength: 100, nullable: true),
+                    Email = table.Column<string>(maxLength: 100, nullable: true),
+                    Password = table.Column<string>(maxLength: 50, nullable: true),
+                    MOBILE = table.Column<string>(maxLength: 20, nullable: true),
+                    Gender = table.Column<int>(nullable: false),
+                    Dob = table.Column<string>(maxLength: 100, nullable: true),
+                    EMAIL_OTP_IN = table.Column<string>(maxLength: 100, nullable: true),
+                    IS_DELETED = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_USER_ASSIGNMENT", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "USER_ASSIGNMENT",
+                schema: "dbo");
+        }
+    }
+}
